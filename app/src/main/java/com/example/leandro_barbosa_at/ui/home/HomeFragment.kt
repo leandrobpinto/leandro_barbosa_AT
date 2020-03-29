9c8ec2cfaa7c6c5beb20d782f6a4fed6fa3e0284
leandro <leandro.bpinto@al.infnet.edu.br>
2020-03-29 17:50:38 -0300

    pulando no primeiro fragment

--- app/src/main/java/com/example/leandro_barbosa_at/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/leandro_barbosa_at/ui/home/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.leandro_barbosa_at.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +24,19 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = root.findViewById(R.id.btnJogar)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnJogar.setOnClickListener {
+            findNavController().navigate(R.id.primeiraFase)
+        }
+
     }
 }
