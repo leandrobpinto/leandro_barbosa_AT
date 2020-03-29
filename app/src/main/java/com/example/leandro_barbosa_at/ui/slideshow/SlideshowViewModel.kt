d51a87d5e22ae6e3f8ad9f00d969bce861f0b447
leandro <leandro.bpinto@al.infnet.edu.br>
2020-03-29 17:43:21 -0300

    colocando fragments

---
+++ app/src/main/java/com/example/leandro_barbosa_at/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.leandro_barbosa_at.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
